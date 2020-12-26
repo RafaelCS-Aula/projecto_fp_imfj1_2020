019a3ed94f98ca657e1d2eea6e13ea63839df1a6
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-12-26 04:02:41 +0000

    Game object: define object behaviour

---
+++ breakout_gameobject.py
@@ -0,0 +1,17 @@
+import pygame
+from abc import abstractmethod
+
+from object3d import Object3d
+
+
+class GameObject(Object3d):
+    """
+    Object in the scene with logic, inherits Object3d
+    """
+        
+    @abstractmethod
+    def setup(self):
+        pass
+    @abstractmethod
+    def update_behaviour(self, delta):
+        pass
