fdf8285caf94e30a7ea99c236f9e4f3b9d8a845c
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2020-12-30 20:24:52 +0000

    Fix bad radius detection

--- bo_collider_sphere.py
+++ bo_collider_sphere.py
@@ -7,17 +7,19 @@ class Sphere_Collider(Collider):
         self.radius = radius
     
     def within_bounds(self, my_position: Vector3, other_point: Vector3):
+        
         distance = (my_position - other_point).magnitude_squared()
-        if distance <= self.radius:
+        if distance < self.radius:
             self.is_colliding = True
-        else:
+        elif distance > self.radius:
             self.is_colliding = False
         return self.is_colliding
             
     def closest_point_on_surface(self, my_position, other_point: Vector3):
+        
         direction = other_point - my_position
         direction.normalize()
         direction *= self.radius
-        closest_point = direction + my_position
+        closest_point = my_position + direction
         return closest_point
     
