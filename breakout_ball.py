b2d17320498ab25f8b56668ea0ada5c6d18a2be6
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-01-05 04:31:03 +0000

    Ball active state input swith and reset

--- breakout_ball.py
+++ breakout_ball.py
@@ -1,3 +1,4 @@
+import pygame
 import math
 
 from breakout_gameobject import GameObject
@@ -9,6 +10,7 @@ from quaternion import Quaternion
 
 from bo_collider_sphere import Sphere_Collider
 
+
 class Ball(GameObject):
     
     ball_speed = 0
@@ -16,9 +18,13 @@ class Ball(GameObject):
     DEFAULT_RADIUS = 0.6
     DEFAULT_SPEED = 3
     SPEED_INCREMENT = 0.5
+    LOWER_LIMIT = -7
+    
+    ACTIVATE_BALL_KEY = pygame.K_SPACE
     
     def __init__(self, name, start_pos, radius = DEFAULT_RADIUS, speed = DEFAULT_SPEED,  color = Color(0,1,1,1)):
         self.radius = Vector3(radius, radius, radius)
+        self.starting_pos = start_pos
         self.position = start_pos
         self.scale = Vector3(1,1,1)
         self.mesh = Mesh.create_sphere(self.radius, 6, 6)
@@ -37,13 +43,17 @@ class Ball(GameObject):
         self.active = False
     
     def setup(self):
-       
-        pass
+       self.reset_ball()
+        
 
     
     def update_behaviour(self, delta):
         if not self.active:
+            keys = pygame.key.get_pressed()
+            if keys[self.ACTIVATE_BALL_KEY]:
+                self.active = True
             return
+        
         self.rotation = Quaternion.AngleAxis(self.up(), 100 * math.radians(delta * self.ball_speed)) * self.rotation
         
         if not self.my_collider.is_colliding:
@@ -51,7 +61,17 @@ class Ball(GameObject):
             self.handled_collisions = []
         # Ball goes forward
         #self.position.z = 0
+        
+        
         self.position += self.ball_speed * self.direction_vector.normalized() * delta
+        
+        #Check input to activate ball
+        
+            
+        
+        if self.position.y < self.LOWER_LIMIT:
+            print("Ball Reset")
+            self.reset_ball()
         
     def handle_collisions(self, collisions: [], delta):
         if not self.active:
@@ -67,6 +87,7 @@ class Ball(GameObject):
                 other_collision_point = col.my_collider.closest_point_on_surface(col.position, self.position)
                 my_collision_point = self.my_collider.closest_point_on_surface(self.position, col.position)
                 
+                # Check the normal of the colision, assuming AABB colisions
                 collision_normal = Vector3(0,0,0)
                 if my_collision_point.x > self.position.x: #right side hit
                     collision_normal = -col.right() #Hit surface is col's left side
@@ -107,7 +128,10 @@ class Ball(GameObject):
                    # Reflection formula
                    #reflectionDirection = ProjectileDirection - 2(ProjectileDirection DOT wallsNormalVector)*wallsNormalVector. 
                    
-                    ricochet = self.direction_vector - 2*(min(1, max(-1,(self.direction_vector.dot(collision_normal.normalized()))))) * collision_normal
+                    
+                    direction_normal_dot = min(1, max(-1,(self.direction_vector.dot(collision_normal)))) 
+                   
+                    ricochet = self.direction_vector - 2 * direction_normal_dot * collision_normal
                    
                     print(ricochet)
                     self.direction_vector = ricochet
@@ -136,6 +160,12 @@ class Ball(GameObject):
             angle (Vector3): Angle in degrees to snap the rotation to
         """
         self.rotation = Quaternion.AngleAxis(Vector3(0,0,1), angle)
+    
+    def reset_ball(self):
+        self.position = self.starting_pos
+        self.active = False
+        self.ball_speed = self.DEFAULT_SPEED
+        self.direction_vector = -self.up()
         
         
         
