a415b25e960eea56522c4269e50122a4270f8665
Rafael Castro e Silva <48124319+RafaelCS-Aula@users.noreply.github.com>
2021-01-05 04:19:38 +0000

    Control flipping with user input

--- breakout_paddle.py
+++ breakout_paddle.py
@@ -6,15 +6,19 @@ from vector3 import Vector3
 
 class Paddle(Block):
     
-    
-    mouse_controlled = True
-    paddle_speed = 1
-    paddle_direction = 0
+
     
     # for mouse input correction
     CAMERA_CORRECTION = 0
     V_RES = 0
     H_RES = 0
+    CONTROLS_FLIP_KEY = pygame.K_q
+    
+    PADDE_SPEED = 6
+    
+        
+    mouse_controlled = False
+    paddle_direction = 0
     
     def setup(self):
         self.my_collider = AABB_Collider(Vector3(self.width, self.height, self.depth))
@@ -30,7 +34,7 @@ class Paddle(Block):
                 self.paddle_direction = -1
             else:
                 self.paddle_direction = 0
-            self.position.x += self.paddle_speed * self.paddle_direction * delta
+            self.position.x += self.PADDE_SPEED * self.paddle_direction * delta
         elif self.mouse_controlled:
             if pygame.mouse.get_focused():
                 mouse_pos = pygame.mouse.get_pos()
@@ -38,7 +42,13 @@ class Paddle(Block):
                 mouse_pos = ((mouse_pos[0] / self.H_RES) * 2 - 1, (mouse_pos[1] / self.V_RES) * 2 - 1)
                 self.position.x = mouse_pos[0] * self.CAMERA_CORRECTION # Multiply by camera's Y position at angle -35
                 #print(pygame.mouse.get_pos()[0])  
+        if keys[self.CONTROLS_FLIP_KEY]:
+            if self.mouse_controlled:
+                self.mouse_controlled = False
+            elif not self.mouse_controlled:
+                self.mouse_controlled = True
                             
     def handle_collisions(self, collisions: [], delta):
-        for c in collisions:
-            print("Paddle HIT by:" + c.name)
+        #for c in collisions:
+         #   print("Paddle HIT by:" + c.name)
+         pass
